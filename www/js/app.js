904de7a3037fe3a75bf5163ec153b691e86044e9
FearDread <ghaptonstall@gmail.com>
2015-10-11 16:06:07 -0700

    --update project files with sidemenu base app and login via ionic build.

--- www/js/app.js
+++ www/js/app.js
@@ -1,53 +1,73 @@
-/*----------------------------\
-|                             |
-\*---------------------------*/
-angular.module('ultimate_totals', [
-    'ionic',
-    'filters',
-    'services',
-    'directives',
-    'controllers'
-])
+// Ionic Starter App
 
-.run(function ($ionicPlatform) {
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
 
-    $ionicPlatform.ready(function () {
-        if (window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        }
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
 
-        if (window.StatusBar) {
-            StatusBar.styleDefault();
-        }
-    });
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
 })
 
-.config(function($ionicConfigProvider, $stateProvider, $urlRouterProvider) {
-
-  $ionicConfigProvider.views.maxCache(0);
-  $ionicConfigProvider.backButton.icon("ion-ios-arrow-left");
-  $ionicConfigProvider.backButton.text("");
-  $ionicConfigProvider.backButton.previousTitleText(false);
-
+.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
-    .state('tab', {
-        url: "/tab",
-        abstract: true,
-        templateUrl: "templates/tabs.html"
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.search', {
+    url: '/search',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/search.html'
+      }
+    }
+  })
+
+  .state('app.browse', {
+      url: '/browse',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/browse.html'
+        }
+      }
+    })
+    .state('app.playlists', {
+      url: '/playlists',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/playlists.html',
+          controller: 'PlaylistsCtrl'
+        }
+      }
     })
 
-    .state('tab.home', {
-        url: "/home",
-        views: {
-            'tab-home': {
-                templateUrl: "templates/tabs/home.html",
-                controller: 'HomeCtrl'
-            }
-        }
-    });
-
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/tab/home');
-
+  .state('app.single', {
+    url: '/playlists/:playlistId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlist.html',
+        controller: 'PlaylistCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/playlists');
 });
