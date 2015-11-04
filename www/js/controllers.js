180a999a0e75233d8b62c0b93307f05dd0c13856
FearDread <ghaptonstall@gmail.com>
2015-11-03 21:14:12 -0800

    --add new ranks.js controller.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,6 +2,7 @@ angular.module('controllers', [
     'controller.app',
     'controller.games',
     'controller.injuries',
+    'controller.ranks',
     'controller.totals',
     'controller.bets'
 ]);
---
+++ www/js/controllers/ranks.js
@@ -0,0 +1,6 @@
+angular
+    .module('controller.ranks', ['ionic', 'ui.router'])
+    .controller('RanksCtrl', function($scope, $state, $stateParams, ApiProvider, StorageProvider, Utils, LoadingService) {
+
+
+    });
